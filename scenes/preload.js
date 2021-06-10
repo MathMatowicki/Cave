1a8d112e67867c265a37319b1e80ca0e811e341a
Mateusz Matowicki <matowicki@gmail.com>
2021-06-10 14:44:59 +0200

    Add progress bar to preload screen

--- scenes/preload.js
+++ scenes/preload.js
@@ -20,10 +20,15 @@ class Preload extends Phaser.Scene {
         this.load.spritesheet('spr-hero', 'spr-hero.png', { frameWidth: 16, frameHight: 16, endFrame: 4, margin: 1, spacing: 2 });
         this.load.spritesheet('spr-slime', 'spr-slime.png', { frameWidth: 16, frameHight: 16, endFrame: 4, margin: 1, spacing: 2 });
         this.load.spritesheet('spr-spider', 'spr-spider.png', { frameWidth: 16, frameHight: 16, endFrame: 4, margin: 1, spacing: 2 });
-    }
 
+        this.load.on('progress', this.onProgress, this);
+    }
     create() {
-        //   this.scene.start()
+        this.time.addEvent({
+            delay: 10000,
+            callback: () => { this.scene.start('Menu'); },
+            callbackScope: this
+        });
     }
 
     createLoadingBar() {
@@ -46,5 +51,27 @@ class Preload extends Phaser.Scene {
             { x: 0.5, y: 1 }
         );
         // Progress bar
+        let x = 10;
+        let y = this.CONFIG.centerY + 5;
+        let w = this.CONFIG.width - 2 * x;
+        let h = 18;
+        this.progress = this.add.graphics({ x: x, y: y });
+        this.border = this.add.graphics({ x: x, y: y });
+        //progress callback
+        this.load.on('progress', this.onProgress, this);
+    }
+    onProgress(val) {
+        //bar
+        let w = this.CONFIG.width - 2 * this.progress.x;
+        let h = 18;
+        this.progress.clear();
+        this.progress.fillStyle('0xFFFFF');
+        this.progress.fillRect(0, 0, w * val, h);
+
+        this.border.clear();
+        this.border.lineStyle(2, '0x4D6592', 1);
+        this.border.strokeRect(0, 0, w * val, h);
+        //%
+        this.txt_progress.setText(Math.round(val * 100) + '%');
     }
 }
