0f5a63020db7167b876328961a2ce7a9d902249c
Mateusz Matowicki <matowicki@gmail.com>
2021-06-10 12:22:59 +0200

    Setup Menu add phaser lib

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const App = function () {
+    'use strict';
+
+    this.VERSION = '0.0.1';
+    this.IS_DEV = true;
+}
+
+App.prototype.start = function () {
+    'use strict';
+
+    //Scenes
+    let scenes = [];
+
+    scenes.push(Boot);
+    scenes.push(Preload);
+    scenes.push(Menu);
+
+    const config = {
+        type: Phaser.AUTO,
+        parent: 'phaser-app',
+        title: 'Cave',
+        width: 360 / 2,
+        height: 640 / 2,
+        scene: scenes,
+        pixelArt: true,
+        backgroundColoer: 0xffffff
+    };
+    //Create game
+    let game = new Phaser.Game(config);
+    game.IS_DEV = this.IS_DEV;
+    game.VERSION = this.VERSION;
+    game.URL = '';
+    game.CONFIG = {
+        width: config.width,
+        height: config.height,
+        centerX: Math.round(0.5 * config.width),
+        centerY: Math.round(0.5 * config.height),
+        tile: 16
+    }
+    game.sound_on = true;
+}
